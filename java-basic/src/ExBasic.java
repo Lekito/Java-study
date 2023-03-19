b469c574fec5b7f2a0576926d8b9dc49f49c444a
Lekito <alexlekito075@gmail.com>
2023-03-19 05:54:21 -0300

    Create basic example in Java

---
+++ java-basic/src/ExBasic.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ExBasic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+
+            System.out.println("Nome: ");
+            nome = sc.next();
+            if (nome.equals("0"))
+                break;
+
+            System.out.println("Idade: ");
+            idade = sc.nextInt();
+        }
+
+        System.out.println("Continua aqui...");
+
+        sc.close();
+    }
+}
