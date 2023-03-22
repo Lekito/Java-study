92bb2002b300a604015fc0621d0c1bd6429b19af
Lekito <alexlekito075@gmail.com>
2023-03-22 14:24:05 -0300

    Adds deposit and withdrawal methods to the main program

--- financeira/src/App.java
+++ financeira/src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Bem vindo Banco Confiança");
         System.out.println("Nome do títular da conta: ");
         String nome = sc.next();
         System.out.println("Número da conta: ");
@@ -14,6 +15,28 @@ public class App {
         Conta cliente = new Conta(numConta, nome, saldo);
 
         cliente.consulta();
+
+        System.out.println(" O que você deseja fazer?");
+        System.out.println("Opção -> 1 Depósito");
+        System.out.println("Opção -> 2 Saque");
+        int opcao = sc.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Valor do depósito: R$ ");
+                double valorDep = sc.nextDouble();
+                cliente.deposito(valorDep);
+                System.out.println("Atualização da sua conta ");
+                cliente.consulta();
+                break;
+            case 2:
+                System.out.println("Valor do saque: R$ ");
+                double valorSaq = sc.nextDouble();
+                cliente.saque(valorSaq);
+                System.out.println("Atualização da sua conta ");
+                cliente.consulta();
+                break;
+        }
 
         sc.close();
     }
