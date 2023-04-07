8d538cc4c04146848b406c316953fab64e3d16c4
Lekito <alexlekito075@gmail.com>
2023-04-07 09:25:09 -0300

    Create ExemploSet

---
+++ escola/src/ExemploSet.java
@@ -0,0 +1,55 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ExemploSet {
+    public static void main(String[] args) {
+        System.out.println("Crie um conjunto e adicione as notas: ");
+        Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        System.out.println(notas.toString()); // não exibiu as notas na ordem correta, elementos repetidos /
+        // só aparecem uma vez (HashSet).
+
+        /*
+         * Nenhum desses métodos no comentário pode ser usado pelo HashSet
+         * 
+         * System.out.println("Exiba a posição da nota 5.0: ");
+         * 
+         * System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+         * 
+         * System.out.println("Substitua a nota 5.0 pela nota 6.0:");
+         */
+
+        System.out.println("Confira se a nota 5.0 está no conjunto: " + notas.contains(5d));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma / notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas.toString());
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if (next < 7)
+                iterator1.remove();
+        }
+
+        System.out.println(notas);
+
+    }
+}
