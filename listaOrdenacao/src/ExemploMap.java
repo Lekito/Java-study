6f9d3e4741d1b0faaeec166fd73bfe49930225ee
Lekito <alexlekito075@gmail.com>
2023-04-09 00:31:15 -0300

    Add methods for sorting and cleaning

--- listaOrdenacao/src/ExemploMap.java
+++ listaOrdenacao/src/ExemploMap.java
@@ -2,8 +2,10 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class ExemploMap {
     public static void main(String[] args) {
@@ -86,6 +88,28 @@ public class ExemploMap {
                 iterator1.remove();
         }
         System.out.println(carrosPopulares);
+
+        System.out.println("Exiba todos os carros na ordem em que foram informados: ");
+        Map<String, Double> carrosPopulares1 = new LinkedHashMap<>() {
+            {
+                put("gol", 14.4);
+                put("uno", 15.6);
+                put("mobi", 16.1);
+                put("hb20", 14.5);
+                put("kwid", 15.6);
+            }
+        };
+        System.out.println(carrosPopulares1.toString());
+
+        System.out.println("----------------------------------------------");
+        System.out.println("Exiba o dicionário ordenado pelo modelo: ");
+        Map<String, Double> carrosPopulares2 = new TreeMap<>(carrosPopulares1);
+        System.out.println(carrosPopulares2.toString());
+        System.out.println("----------------------------------------------");
+
+        System.out.println("Apague o dicionarios de carros: ");
+        carrosPopulares.clear();
+        System.out.println("Confira se o dicionário está vazio: " + carrosPopulares.isEmpty());
 
     }
 }
